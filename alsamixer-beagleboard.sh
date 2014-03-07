999b63352386aaec5a97f414f609280b61a01cc5
sunduchkov <sunduchkov@yahoo.com>
2014-03-07 10:10:39 -0500

    added alsa mixer setup for Beagleboard xM

---
+++ alsamixer-beagleboard.sh
@@ -0,0 +1,10 @@
+amixer sset 'Analog' 1 #
+amixer sset 'Analog Left AUXL' cap
+amixer sset 'Analog Right AUXR' cap
+amixer sset 'TX1' 'Analog'
+amixer sset 'TX1 Digital' 5
+
+amixer sset 'HeadsetL Mixer AudioL1' on
+amixer sset 'HeadsetR Mixer AudioR1' on
+amixer sset 'Headset' 2
+
