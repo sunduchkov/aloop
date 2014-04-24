d51d225e7d8921e1e777af49fb8eeb65d40d19e6
sunduchkov <sunduchkov@yahoo.com>
2014-04-24 12:36:18 -0400

    update

--- aloop/src/aloop.c
+++ aloop/src/aloop.c
@@ -20,6 +20,8 @@ typedef struct
 	getparams_t		params;
 	int				loop;
 
+	int 			gain;
+
 }	aadsp_t;
 
 void setscheduler(void)
@@ -65,10 +67,12 @@ static void ProcessStereo(int* samplesIn[2], int* samplesOut[2], int N)
 	}
 }
 #else
-static void ProcessStereo(int* samplesIn, int* samplesOut, int N)
+static void ProcessStereo(aadsp_t* p, int* samplesIn, int* samplesOut, int N)
 {
 	int i;
 	int L, R;
+	int64_t acc;
+
 	//static int k = 0;
 	for(i = 0; i < N; ++i)
 	{
@@ -84,6 +88,11 @@ static void ProcessStereo(int* samplesIn, int* samplesOut, int N)
 		// Spectrum inversion for Right channel
 		//if(i & 1) R = -R;
 
+		acc = (int64_t)L * p->gain;
+		L = (int)(acc >> 31);
+		acc = (int64_t)R * p->gain;
+		R = (int)(acc >> 31);
+
 		samplesOut[i*2] = L;
 		samplesOut[i*2+1] = R;
 	}
@@ -94,109 +103,133 @@ static void* realtime_audio(void* p)
 {
 	aadsp_t* aadsp = (aadsp_t*)p;
 	alsa_driver_t* driver = &aadsp->driver;
-	int r, w;
-	int avail;
+	int* addr;
+	int size;
 
-	if(0 == alsa_driver_new(driver))
+	while(1)
 	{
-		printf("alsa_driver_new not succeeded\n");
-		return 0;
-	}
-
-	printf("Audio Interface \"%s\" initialized with %d [ms] latency\n", driver->alsa_driver_name, driver->latency);
-
-	if(!alsa_driver_prepare(driver))
-	{
-		printf("alsa_driver_start not succeeded\n");
-		return 0;
-	}
-
-	printf("Audio Interface prepared for start\n");
-
-	alsa_driver_start(driver);
-
-	r = 0;
-	w = 0;
-
-	aadsp->loop = 1;
-
-	while (aadsp->loop)
-	{
-		alsa_driver_wait(driver);
-
-		avail = alsa_driver_read(driver);
-		r += avail;
-		if(r >= driver->sample_rate)
-		{
-			printf(".");
-			fflush(stdout);
-			r = 0;
+		if(ALSA_STATUS_OK != alsa_driver_prepare(driver)) {
+			printf("alsa_driver_prepare not succeeded\n");
+			continue;
 		}
 
-		avail = alsa_driver_write(driver, ProcessStereo);
+		printf("Audio Interface prepared for start\n");
 
-		w += avail;
-		if(w >= driver->sample_rate)
-		{
-			printf("+");
-			fflush(stdout);
-			w = 0;
+		if(ALSA_STATUS_OK != alsa_driver_start(driver)) {
+			printf("alsa_driver_start not succeeded\n");
+			continue;
 		}
+
+		aadsp->loop = 1;
+
+		aadsp->gain = 0x7fffffff;
+
+		while (aadsp->loop)
+		{
+			if(ALSA_STATUS_OK != alsa_driver_wait(driver, NULL)) {
+				break;
+			}
+
+			if(ALSA_STATUS_OK != alsa_driver_read(driver)) {
+				break;
+			}
+
+			if(ALSA_STATUS_OK != alsa_driver_write_prepare(driver, &addr, &size)) {
+				break;
+			}
+
+			ProcessStereo(aadsp, addr, addr, size);
+
+			if(ALSA_STATUS_OK != alsa_driver_write(driver)) {
+				break;
+			}
+		}
+
+		printf("ALSA restart\n");
 	}
 
 	return NULL;
 }
 
+const char* pthread_err(int err)
+{
+	static const char sEAGAIN[] = "Insufficient  resources  to  create another thread";
+	static const char sEINVAL[] = "Invalid settings";
+	static const char sEPERM[] = "No permission to set the scheduling policy and parameters";
+	static const char sESRCH[] = "No thread with the ID thread could be found";
+	static const char sENOTSUP[] = "Unsupported value";
+	static const char sUNKNOWN[] = "Unrecognized error";
+	const char* str = NULL;
+
+	if(EAGAIN == err) {
+		str = sEAGAIN;
+	}
+	else if(EINVAL == err) {
+		str = sEINVAL;
+	}
+	else if(EPERM == err) {
+		str = sEPERM;
+	}
+	else if(ESRCH == err) {
+		str = sESRCH;
+	}
+	else if(ENOTSUP == err) {
+		str = sENOTSUP;
+	}
+	else {
+		str = sUNKNOWN;
+	}
+
+	return str;
+}
+
 int main(int argc, char *argv[])
 {
 	aadsp_t aadsp;
-
+	alsa_driver_t* driver = &aadsp.driver;
 	int err;
 
 	printf("ALSA Path-through starting\n");
 
-	if(0 == (err = alsa_driver_get_options(&aadsp.driver, argc, argv)))
-	{
+	if(ALSA_STATUS_OK != alsa_driver_get_options(&aadsp.driver, argc, argv)) {
 		exit(1);
 	}
+
+	if(ALSA_STATUS_OK != alsa_driver_open(driver)) {
+		printf("alsa_driver_new not succeeded\n");
+		exit(1);
+	}
+
+	printf("Audio Interface \"%s\" initialized with %d [ms] latency\n", driver->alsa_driver_name, driver->latency);
 
 #if REALTIMEAUDIO_ENABLED
 	pthread_t thread;
 	pthread_attr_t attr;
 
-	if(0 != pthread_attr_init(&attr)) {
-		printf("pthread_attr_init: error");
+	if(0 != (err = pthread_attr_init(&attr))) {
+		printf("pthread_attr_init: error (%s)\n", pthread_err(err));
 		exit(1);
 	}
 
-	if(0 != pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED)) {
-		printf("pthread_attr_setdetachstate: error");
+	if(0 != (err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED))) {
+		printf("pthread_attr_setdetachstate: error (%s)\n", pthread_err(err));
 		exit(1);
 	}
 
-	if(0 != pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED)) {
-		printf("pthread_attr_setinheritsched: error");
+	if(0 != (err = pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED))) {
+		printf("pthread_attr_setinheritsched: error (%s)\n", pthread_err(err));
 		exit(1);
 	}
 
-	if(0 != (err = pthread_create(&thread, &attr, realtime_audio, &aadsp)))
-	{
-		if(EAGAIN == err) {
-			printf("pthread_create: \n");
-		}
-		if(EINVAL == err) {
-			printf("pthread_create: Invalid settings in attr\n");
-		}
-		if(EPERM == err) {
-			printf("pthread_create:No permission to set the scheduling policy and parameters specified in attr\n");
-		}
+	if(0 != (err = pthread_create(&thread, &attr, realtime_audio, &aadsp)))	{
+		printf("pthread_create: error (%s)\n", pthread_err(err));
 		exit(1);
 	}
 
 	struct sched_param param;
 	param.sched_priority = sched_get_priority_max(SCHED_FIFO) - 10;
-	if(0 != pthread_setschedparam(thread, SCHED_FIFO, &param)) {
-		printf("pthread_setschedparam: error\n");
+	if(0 != (err = pthread_setschedparam(thread, SCHED_FIFO, &param))) {
+		printf("pthread_setschedparam: error (%s)\n", pthread_err(err));
 		exit(1);
 	}
 #endif
@@ -214,6 +247,11 @@ int main(int argc, char *argv[])
 
 		if(getparams_get(&aadsp.params)) {
 			printf("%d %f\n", aadsp.params.nNumber, aadsp.params.fValue);
+
+			if(7 == aadsp.params.nNumber) {
+				aadsp.gain = (int)((int64_t)0x7fffffff * aadsp.params.fValue / 100);
+				printf("%x\n", aadsp.gain);
+			}
 		}
 	}
 
@@ -225,6 +263,8 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 #endif
+
+	alsa_driver_close(driver);
 
 	return 0;
 }
